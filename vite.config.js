b44d48e003ba4b1b7cc363b3bbd23e3ff83512cf
Clayton Wellborne <wellborneclayton@gmail.com>
2025-08-16 01:55:43 -0500

    Use custom domain weareproject18.com (CNAME) and set Vite base=/

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/youth-council-site/',
+  base: '/',   // ⬅️ important: was '/youth-council-site/'
 })
