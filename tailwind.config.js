534ccb8b8a5ec611eb50f2075260f79b40515024
Clayton Wellborne <wellborneclayton@gmail.com>
2025-08-19 16:32:45 -0500

    Move Tailwind to PostCSS (Vite): add config, update index.html, package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-export default { darkMode: 'media',
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
